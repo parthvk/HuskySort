86d2fd1d0eabfa9a15cc652751abc50dcdf70ad8
Parth Vasant Kataria <kataria.p@northeastern.edu>
2022-08-09 08:46:52 -0400

    shell sort and quick sort

--- src/main/java/edu/neu/coe/huskySort/sort/simple/ShellSort.java
+++ src/main/java/edu/neu/coe/huskySort/sort/simple/ShellSort.java
@@ -2,11 +2,20 @@
   (c) Copyright 2018, 2019 Phasmid Software
  */
 package edu.neu.coe.huskySort.sort.simple;
-
 import edu.neu.coe.huskySort.sort.ComparableSortHelper;
 import edu.neu.coe.huskySort.sort.ComparisonSortHelper;
+import edu.neu.coe.huskySort.sort.HelperFactory;
 import edu.neu.coe.huskySort.sort.SortWithHelper;
 import edu.neu.coe.huskySort.util.Config;
+import edu.neu.coe.huskySort.util.Instrumenter;
+import edu.neu.coe.huskySort.util.PrivateMethodInvoker;
+import edu.neu.coe.huskySort.util.StatPack;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
 
 /**
  * Class to implement Shell Sort.
@@ -14,6 +23,37 @@ import edu.neu.coe.huskySort.util.Config;
  * @param <X> the type of element on which we will be sorting (must implement Comparable).
  */
 public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
+
+    /**
+     * Constructor for ShellSort.
+     * <p>
+     * NOTE: not used.
+     *
+     * @param N      the number elements we expect to sort.
+     * @param config the configuration.
+     */
+    public ShellSort(final int m, final int N, final Config config) {
+        super(DESCRIPTION, N, config);
+        this.m = m;
+    }
+
+    public ShellSort() throws IOException {
+        this(5);
+    }
+
+    /**
+     * Constructor for ShellSort
+     *
+     * @param m the "gap" (h) sequence to follow:
+     *          1: ordinary insertion sort;
+     *          2: use powers of two less one;
+     *          3: use the sequence based on 3 (the one in the book): 1, 4, 13, etc.
+     *          4: Sedgewick's sequence (not implemented).
+     */
+    public ShellSort(final int m) {
+        this(m, new ComparableSortHelper<>(DESCRIPTION));
+    }
+
 
     /**
      * Method to sort a sub-array of an array of Xs.
@@ -34,18 +74,7 @@ public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
     public static final String DESCRIPTION = "Shell sort";
 
-    /**
-     * Constructor for ShellSort.
-     * <p>
-     * NOTE: not used.
-     *
-     * @param N      the number elements we expect to sort.
-     * @param config the configuration.
-     */
-    public ShellSort(final int m, final int N, final Config config) {
-        super(DESCRIPTION, N, config);
-        this.m = m;
-    }
+
 
     /**
      * Constructor for ShellSort
@@ -57,7 +86,7 @@ public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
      *               4: Sedgewick's sequence (not implemented).
      * @param helper an explicit instance of ComparisonSortHelper to be used.
      */
-    public ShellSort(final int m, final ComparableSortHelper<X> helper) {
+    public ShellSort(final int m, final ComparisonSortHelper<X> helper) {
         super(helper);
         this.m = m;
     }
@@ -69,10 +98,24 @@ public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
      *          1: ordinary insertion sort;
      *          2: use powers of two less one;
      *          3: use the sequence based on 3 (the one in the book): 1, 4, 13, etc.
-     *          4: Sedgewick's sequence (not implemented).
+     *          4: Sedgewick's sequence.
+     *          5: Pratt Sequence 2^i*3^j with i, j >= 0.
      */
-    public ShellSort(final int m) {
+    public ShellSort(int m, Config config) {
         this(m, new ComparableSortHelper<>(DESCRIPTION));
+    }
+
+
+
+    /**
+     * Set the "shell" function which is invoked on the helper after each shell (i.e. each value of h).
+     * Yes, I do realize that shell was the name of the inventor, Donald Shell.
+     * But it's also a convenient name of a (set of) h-sorts which one particular h-value.
+     *
+     * @param shellFunction a consumer of Helper of X.
+     */
+    public void setShellFunction(Consumer<ComparisonSortHelper<X>> shellFunction) {
+        this.shellFunction = shellFunction;
     }
 
     /**
@@ -93,13 +136,17 @@ public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
 
     private final int m;
 
+    private Consumer<ComparisonSortHelper<X>> shellFunction = null;
+
     /**
      * Private inner class to provide h (gap) values.
      */
-    private class H {
+    class H {
         @SuppressWarnings("CanBeFinal")
         private int h = 1;
+        private int i;
         private boolean started = false;
+        final List<Integer> data = new ArrayList<>();
 
         H(final int N) {
             switch (m) {
@@ -110,6 +157,28 @@ public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
                     break;
                 case 3:
                     while (h <= N / 3) h = h * 3 + 1;
+                    break;
+                case 4:
+                    i = 0;
+                    while (sedgewick(i) < N) i++;
+                    i--;
+                    h = (int) sedgewick(i); // Note there will be loss of precision for large i
+                    break;
+                case 5:
+                    //2^i*3^j with i, j >= 0
+                    int i;
+                    int j = 1;
+                    while (j <= N) {
+                        i = j;
+                        while (i <= N) {
+                            data.add(i);
+                            i = i * 2;
+                        }
+                        j = j * 3;
+                    }
+                    Collections.sort(data);
+                    this.i = data.size() - 1;
+                    h = data.get(this.i);
                     break;
                 default:
                     throw new RuntimeException("invalid m value: " + m);
@@ -145,12 +214,155 @@ public class ShellSort<X extends Comparable<X>> extends SortWithHelper<X> {
                     case 3:
                         h = h / 3;
                         return h;
+                    case 4:
+                        i--;
+                        return (int) sedgewick(i);
+                    case 5:
+                        i--;
+                        if (i < 0) {
+                            return 0;
+                        }
+                        return data.get(i);
                     default:
                         throw new RuntimeException("invalid m value: " + m);
                 }
             } else {
                 started = true;
                 return h;
+            }
+        }
+
+
+        long sedgewick(int k) {
+            if (k < 0) return 0;
+            if (k % 2 == 0) return 9L * (powerOf2(k) - powerOf2(k / 2)) + 1;
+            else return 8L * powerOf2(k) - 6 * powerOf2((k + 1) / 2) + 1;
+        }
+
+        private long powerOf2(int k) {
+            long value = 1;
+            for (int i = 0; i < k; i++) value *= 2;
+            return value;
+        }
+    }
+
+    /**
+     * Method to run quicksort and get values for compares
+     * and swaps for different array sizes using statpack.
+     */
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Please enter a number: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if(num==1){
+            int k = 10;
+            System.out.println("test");
+            Integer[] sizeOfArray = new Integer[k];
+            for(int i=0;i<k;i++){
+                sizeOfArray[i] = (int)Math.pow(2, 7+i);
+            }
+            Integer cycles = 1;
+            System.out.println("Averaging across " + cycles + " runs");
+            for (Integer n : sizeOfArray) {
+                double swaps = 0.0;
+                double compares = 0.0;
+                for (int t = 0; t < cycles; t++) {
+                    final Config config = Config.setupConfig("true", "", "", "", "");
+                    final ComparisonSortHelper<Integer> helper = HelperFactory.create("quick sort", n, config);
+                    Integer[] xs = new Integer[n] ;
+                    for(int i = 0;i<n;i++){
+                        xs[i]=i;
+                    }
+                    ShellSort<Integer> s = new ShellSort(3,helper);
+                    s.init(n);
+                    helper.preProcess(xs);
+                    Integer[] ys = s.sort(xs);
+                    helper.postProcess(ys);
+                    final PrivateMethodInvoker privateMethodTester = new PrivateMethodInvoker(helper);
+                    final StatPack statPack = (StatPack) privateMethodTester.invokePrivate("getStatPack");
+                    swaps = swaps + statPack.getStatistics(Instrumenter.SWAPS).mean();
+                    compares = compares + statPack.getStatistics(Instrumenter.COMPARES).mean();
+                    System.out.println(statPack);
+                }
+
+                double averageSwap = swaps / cycles;
+                double averageCompare = compares / cycles;
+                double ratio =  averageCompare/averageSwap ;
+                System.out.println( "-------------------------------------------------------------------------------------------------------");
+                System.out.println( "Array Size: " + n + " Avg Swap: " + averageSwap + " Avg Compare: " + averageCompare + " Ratio: " + ratio);
+            }
+        }
+        if(num==2){
+            int k = 10;
+            Integer[] sizeOfArray = new Integer[k];
+            for(int i=0;i<k;i++){
+                sizeOfArray[i] = (int)Math.pow(2, 7+i);
+            }
+            Integer cycles = 1;
+            System.out.println("Averaging across " + cycles + " runs");
+            for (Integer n : sizeOfArray) {
+                double swaps = 0.0;
+                double compares = 0.0;
+                for (int t = 0; t < cycles; t++) {
+                    final Config config = Config.setupConfig("true", "", "", "", "");
+                    final ComparisonSortHelper<Integer> helper = HelperFactory.create("quick sort", n, config);
+                    final Integer[] xs = helper.random(Integer.class, r -> r.nextInt(n));
+                    ShellSort<Integer> s = new ShellSort(3,helper);
+                    s.init(n);
+                    helper.preProcess(xs);
+                    Integer[] ys = s.sort(xs);
+                    helper.postProcess(ys);
+                    final PrivateMethodInvoker privateMethodTester = new PrivateMethodInvoker(helper);
+                    final StatPack statPack = (StatPack) privateMethodTester.invokePrivate("getStatPack");
+                    swaps = swaps + statPack.getStatistics(Instrumenter.SWAPS).mean();
+                    compares = compares + statPack.getStatistics(Instrumenter.COMPARES).mean();
+                    System.out.println(statPack);
+                }
+
+                double averageSwap = swaps / cycles;
+                double averageCompare = compares / cycles;
+                double ratio =  averageCompare/averageSwap ;
+                System.out.println( "-------------------------------------------------------------------------------------------------------");
+                System.out.println( "Array Size: " + n + " Avg Swap: " + averageSwap + " Avg Compare: " + averageCompare + " Ratio: " + ratio);
+            }
+        }
+        if(num==3){
+            int k = 10;
+            Integer[] sizeOfArray = new Integer[k];
+            for(int i=0;i<k;i++){
+                sizeOfArray[i] = (int)Math.pow(2, 7+i);
+            }
+            Integer cycles = 1;
+            System.out.println("Averaging across " + cycles + " runs");
+            for (Integer n : sizeOfArray) {
+                double swaps = 0.0;
+                double compares = 0.0;
+                for (int t = 0; t < cycles; t++) {
+                    final Config config = Config.setupConfig("true", "", "", "", "");
+                    final ComparisonSortHelper<Integer> helper = HelperFactory.create("quick sort", n, config);
+                    Integer[] xs = new Integer[n] ;
+                    for(int i = 0;i<n;i++){
+                        xs[i]=n-i;
+                    }
+                    ShellSort<Integer> s = new ShellSort(3,helper);
+                    s.init(n);
+                    helper.preProcess(xs);
+                    Integer[] ys = s.sort(xs);
+                    helper.postProcess(ys);
+                    final PrivateMethodInvoker privateMethodTester = new PrivateMethodInvoker(helper);
+                    final StatPack statPack = (StatPack) privateMethodTester.invokePrivate("getStatPack");
+                    swaps = swaps + statPack.getStatistics(Instrumenter.SWAPS).mean();
+                    compares = compares + statPack.getStatistics(Instrumenter.COMPARES).mean();
+                    System.out.println(statPack);
+                }
+
+                double averageSwap = swaps / cycles;
+                double averageCompare = compares / cycles;
+                double ratio =  averageCompare/averageSwap ;
+                System.out.println( "-------------------------------------------------------------------------------------------------------");
+                System.out.println( "Array Size: " + n + " Avg Swap: " + averageSwap + " Avg Compare: " + averageCompare + " Ratio: " + ratio);
             }
         }
     }
--- src/test/java/edu/neu/coe/huskySort/sort/simple/ShellSortTest.java
+++ src/test/java/edu/neu/coe/huskySort/sort/simple/ShellSortTest.java
@@ -3,22 +3,54 @@
  */
 
 package edu.neu.coe.huskySort.sort.simple;
-
 import edu.neu.coe.huskySort.sort.ComparableSortHelper;
+import edu.neu.coe.huskySort.sort.ComparisonSortHelper;
+import edu.neu.coe.huskySort.sort.HelperFactory;
 import edu.neu.coe.huskySort.sort.Sort;
-import edu.neu.coe.huskySort.util.PrivateMethodInvoker;
+import edu.neu.coe.huskySort.util.*;
+import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
+import java.io.IOException;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("ALL")
 public class ShellSortTest {
 
     @Test
+    public void testSedgewick0() throws IOException {
+        ShellSort<Integer> shellSort = new ShellSort<>(4);
+        ShellSort<Integer>.H h = shellSort.new H(0);
+        assertEquals(1, h.sedgewick(0));
+        assertEquals(5, h.sedgewick(1));
+        assertEquals(19, h.sedgewick(2));
+        assertEquals(41, h.sedgewick(3));
+    }
+
+    @Test
+    public void testSedgewick50() throws IOException {
+        ShellSort<Integer> shellSort = new ShellSort<>(4);
+        ShellSort<Integer>.H h = shellSort.new H(50);
+        assertEquals(41, h.first());
+        assertEquals(19, h.next());
+        assertEquals(5, h.next());
+        assertEquals(1, h.next());
+        assertEquals(0, h.next());
+    }
+
+    @BeforeClass
+    public static void setupClass() {
+        try {
+            config = Config.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void hSort3() {
-        ShellSort<Integer> sorter = new ShellSort<>(3);
+        ShellSort<Integer> sorter = new ShellSort<>(3,config);
         PrivateMethodInvoker t = new PrivateMethodInvoker(sorter);
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {4, 1, -1, 2, 8, 3, 0, 5, 15, 6, 1, 9, 17, 7, 11};
@@ -31,14 +63,30 @@ public class ShellSortTest {
     public void sort1() throws Exception {
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
-        assertArrayEquals(zs, new ShellSort<Integer>(3).sort(xs));
+        assertArrayEquals(zs, new ShellSort<Integer>(3,config).sort(xs));
+    }
+
+    @Test
+    public void sort1a() throws Exception {
+        Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
+        Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
+        assertArrayEquals(zs, new ShellSort<Integer>(5,config).sort(xs));
     }
 
     @Test
     public void sort2() throws Exception {
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
-        Sort ss = new ShellSort<Integer>(3);
+        Sort ss = new ShellSort<Integer>(3,config);
+        ss.sort(xs, 0, xs.length);
+        assertArrayEquals(zs, xs);
+    }
+
+    @Test
+    public void sort2a() throws Exception {
+        Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
+        Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
+        Sort ss = new ShellSort<Integer>(5,config);
         ss.sort(xs, 0, xs.length);
         assertArrayEquals(zs, xs);
     }
@@ -47,7 +95,16 @@ public class ShellSortTest {
     public void sort3() throws Exception {
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {15, -1, 3, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
-        Sort ss = new ShellSort<Integer>(3);
+        Sort ss = new ShellSort<Integer>(3,config);
+        ss.sort(xs, 1, 3);
+        assertArrayEquals(zs, xs);
+    }
+
+    @Test
+    public void sort3a() throws Exception {
+        Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
+        Integer[] zs = {15, -1, 3, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
+        Sort ss = new ShellSort<Integer>(5, config);
         ss.sort(xs, 1, 3);
         assertArrayEquals(zs, xs);
     }
@@ -56,7 +113,16 @@ public class ShellSortTest {
     public void sort4() throws Exception {
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 7, 9, 11, 17};
-        Sort ss = new ShellSort<Integer>(3);
+        Sort ss = new ShellSort<Integer>(3,config);
+        ss.sort(xs, 11, xs.length);
+        assertArrayEquals(zs, xs);
+    }
+
+    @Test
+    public void sort4a() throws Exception {
+        Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
+        Integer[] zs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 7, 9, 11, 17};
+        Sort ss = new ShellSort<Integer>(5, config);
         ss.sort(xs, 11, xs.length);
         assertArrayEquals(zs, xs);
     }
@@ -73,18 +139,96 @@ public class ShellSortTest {
     }
 
     @Test
+    public void sort7() throws Exception {
+        doShellSortTest(1000, 3);
+    }
+
+    @Test
+    public void sort7a() throws Exception {
+        doShellSortTest(1000, 5);
+    }
+
+    @Test
+    public void sort5a() throws Exception {
+        doShellSortTest(10, 3);
+    }
+
+    @Test
+    public void sort5b() throws Exception {
+        doShellSortTest(10, 5);
+    }
+
+    @Test
+    public void sort6a() throws Exception {
+        doShellSortTest(100, 3);
+    }
+
+    @Test
+    public void sort6b() throws Exception {
+        doShellSortTest(100, 5);
+    }
+
+    private void doShellSortTest(int N, final int gapSequence) throws IOException {
+        final Config config = ConfigTest.setupConfig("true", "", "1", "", "");
+        ComparisonSortHelper<Integer> helper = (ComparisonSortHelper<Integer>) HelperFactory.create("Shell sort", N, config);
+        System.out.println(helper);
+        final Integer[] xs = helper.random(Integer.class, r -> r.nextInt(N * 2));
+        helper.init(N);
+        helper.preProcess(xs);
+        ShellSort<Integer> sorter = new ShellSort<>(gapSequence, helper);
+        sorter.mutatingSort(xs);
+        helper.postProcess(xs);
+        assertTrue(helper.sorted(xs));
+        showStatistics(helper);
+    }
+
+    private void showStatistics(ComparisonSortHelper<Integer> helper) {
+        if (ComparisonSortHelper.class.isAssignableFrom(helper.getClass())) {
+            final PrivateMethodInvoker privateMethodTester = new PrivateMethodInvoker(helper);
+            final StatPack statPack = (StatPack) privateMethodTester.invokePrivate("getStatPack");
+            double inversions = statPack.getStatistics(Instrumenter.INVERSIONS).mean();
+            double compares = statPack.getStatistics(Instrumenter.COMPARES).mean();
+            double swaps = statPack.getStatistics(Instrumenter.SWAPS).mean();
+            double fixes = statPack.getStatistics(Instrumenter.FIXES).mean();
+            System.out.println(inversions + ", " + compares + ", " + swaps + " " + fixes);
+            System.out.println("inversions: " + inversions);
+            System.out.println("compares: " + swaps);
+            System.out.println("swaps: " + fixes);
+        }
+    }
+
+//    private void showInversions(ComparisonSortHelper<Integer> helper) {
+//        if (ComparisonSortHelper.class.isAssignableFrom(helper.getClass())) {
+//            ComparisonSortHelper<Integer> instrumentedHelper = (ComparisonSortHelper<Integer>) helper;
+//            System.out.println("inversions: " + instrumentedHelper.inversions(Instrumenter.getRandomArray()));
+//            System.out.println("compares: " + instrumentedHelper.getCompares());
+//            System.out.println("swaps: " + instrumentedHelper.getSwaps());
+//        }
+//    }
+
+    @Test
     public void sortH1() {
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
-        assertArrayEquals(zs, new ShellSort<Integer>(1).sort(xs));
+        assertArrayEquals(zs, new ShellSort<Integer>(1,config).sort(xs));
     }
 
     @Test
     public void sortH2() {
         Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
         Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
-        assertArrayEquals(zs, new ShellSort<Integer>(2).sort(xs));
+        assertArrayEquals(zs, new ShellSort<Integer>(2,config).sort(xs));
+    }
+
+    @Test
+    public void sortH3() {
+        Integer[] xs = {15, 3, -1, 2, 4, 1, 0, 5, 8, 6, 1, 9, 17, 7, 11};
+        Integer[] zs = {-1, 0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 15, 17};
+        assertArrayEquals(zs, new ShellSort<Integer>(5, config).sort(xs));
     }
 
 
+    final static LazyLogger logger = new LazyLogger(ShellSort.class);
+
+    private static Config config;
 }
